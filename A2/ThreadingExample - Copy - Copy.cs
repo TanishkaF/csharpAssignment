77fd4ff39dd792e5d8cf9cd385eabebfbbcf97df
TanishkaF <tanishkaf@mindfiresolutions.com>
2024-01-24 18:02:21 +0530

    Praticed MultiThreading.

---
+++ A2/ThreadingExample - Copy - Copy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Started......");
+
+            SomeMethod();
+
+            Console.WriteLine("Main Method End");
+            Console.ReadKey();
+        }
+
+       // public static async void SomeMethod()
+        public  static void SomeMethod()
+        {
+            Console.WriteLine("Some Method Started......");
+
+           Thread.Sleep(TimeSpan.FromSeconds(10));
+//await Task.Delay(TimeSpan.FromSeconds(10));
+            Console.WriteLine("\n");
+            Console.WriteLine("Some Method End");
+        }
+    }
+}
---
+++ A2/ThreadingExample - Copy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace MutexDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Mutex mutex = new Mutex(false, "MutexDemo"))
+            {
+                //Checking if Other External Thread is Running
+                if (!mutex.WaitOne(5000, false))
+                {
+                    Console.WriteLine("An Instance of the Application is Already Running");
+                    Console.ReadKey();
+                    return;
+                }
+                Console.WriteLine("Application Is Running.......");
+                Console.ReadKey();
+            }
+        }
+    }
+}
